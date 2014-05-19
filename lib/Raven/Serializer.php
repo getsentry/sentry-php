2351d97c862f02343e5465d550df68bd9632cad9
David Cramer <dcramer@gmail.com>
2014-05-19 01:56:54 -0700

    Bump max_depth to 9 to better support chained exceptions

--- lib/Raven/Serializer.php
+++ lib/Raven/Serializer.php
@@ -30,7 +30,7 @@ class Raven_Serializer
      * Serialize an object (recursively) into something safe for data
      * sanitization and encoding.
      */
-    public static function serialize($value, $max_depth=7, $_depth=0)
+    public static function serialize($value, $max_depth=9, $_depth=0)
     {
         if (is_object($value) || is_resource($value)) {
             return self::serializeValue($value);
