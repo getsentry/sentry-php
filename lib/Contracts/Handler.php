ebf55abde1b7d0c48f7a68d3d82cd305d5183ea0
Amelia Ikeda <amelia@dorks.io>
2015-04-03 20:13:06 +0100

    Add a Handler contract for all error handlers

---
+++ lib/Contracts/Handler.php
@@ -0,0 +1,38 @@
+<?php namespace Raven\Contracts;
+
+use Exception;
+
+/**
+ * Contract for Error Handlers
+ *
+ * @package raven
+ */
+interface Handler
+{
+    /**
+     * A method compatible with set_error_handler
+     *
+     * @param int    $code
+     * @param string $message
+     * @param string $file
+     * @param int    $line
+     * @param array  $context
+     * @return mixed
+     */
+    public function handleError($code, $message, $file = '', $line = 0, $context = []);
+
+    /**
+     * Handle a fatal error as a special case.
+     *
+     * @return void
+     */
+    public function handleFatalError();
+
+    /**
+     * @param \Exception $e       The exception thrown
+     * @param bool       $isError True if $e is an ErrorException
+     * @param null       $vars    Variables to pass to sentry
+     * @return void
+     */
+    public function handleException(Exception $e, $isError = false, $vars = null);
+}
