b17196c86e9248511bb9723be7b17cc329f13897
David Cramer <dcramer@gmail.com>
2016-05-03 13:00:39 -0700

    Kill bad output

--- lib/Raven/Autoloader.php
+++ lib/Raven/Autoloader.php
@@ -36,7 +36,6 @@ class Raven_Autoloader
             return;
         }
 
-        var_dump(dirname(__FILE__) . '/../' . str_replace(array('_', "\0"), array('/', ''), $class) . '.php');
         if (is_file($file = dirname(__FILE__).'/../'.str_replace(array('_', "\0"), array('/', ''), $class).'.php')) {
             require $file;
         }
