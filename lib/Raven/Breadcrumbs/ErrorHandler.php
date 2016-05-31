be1c720b4c0a638b95d28a0f9970aad9e85ca8e4
David Cramer <dcramer@gmail.com>
2016-05-31 11:09:31 -0700

    Re-use existing code for severity

--- lib/Raven/Breadcrumbs/ErrorHandler.php
+++ lib/Raven/Breadcrumbs/ErrorHandler.php
@@ -19,72 +19,12 @@ class Raven_Breadcrumbs_ErrorHandler
         $this->ravenClient = $ravenClient;
     }
 
-    public function codeToName($code)
-    {
-        switch ($code) {
-            case 1:
-                return 'E_ERROR';
-            case 2:
-                return 'E_WARNING';
-            case 4:
-                return 'E_PARSE';
-            case 8:
-                return 'E_NOTICE';
-            case 16:
-                return 'E_CORE_ERROR';
-            case 32:
-                return 'E_CORE_WARNING';
-            case 64:
-                return 'E_COMPILE_ERROR';
-            case 128:
-                return 'E_COMPILE_WARNING';
-            case 256:
-                return 'E_USER_ERROR';
-            case 512:
-                return 'E_USER_WARNING';
-            case 1024:
-                return 'E_USER_NOTICE';
-            case 2048:
-                return 'E_STRICT';
-            case 4096:
-                return 'E_RECOVERABLE_ERROR';
-            case 8192:
-                return 'E_DEPRECATED';
-            case 16384:
-                return 'E_USER_DEPRECATED';
-            case 30719:
-                return 'E_ALL';
-            default:
-                return 'E_UNKNOWN';
-        }
-    }
-
-    public function codeToLevel($code)
-    {
-        switch ($code) {
-            case 1:
-            case 4:
-            case 16:
-            case 64:
-            case 256:
-            case 4096:
-                return 'error';
-            case 2:
-            case 32:
-            case 128:
-            case 512:
-                return 'warning';
-            default:
-                return 'info';
-        }
-    }
-
     public function handleError($code, $message, $file = '', $line = 0, $context=array())
     {
         $this->ravenClient->breadcrumbs->record(array(
             'category' => 'error_reporting',
             'message' => $message,
-            'level' => $this->codeToLevel($code),
+            'level' => $this->ravenClient->translateSeverity($code),
             'data' => array(
                 'code' => $code,
                 'line' => $line,
