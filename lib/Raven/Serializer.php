70feae641997674a3ca2bb0b90e363cbf06b67bd
Tobias Nitsche <t.nitsche@checkdomain.de>
2016-07-13 14:22:00 +0200

    Fix: PHP-CS

--- lib/Raven/Serializer.php
+++ lib/Raven/Serializer.php
@@ -31,10 +31,7 @@ class Raven_Serializer
      *
      * @see http://php.net/manual/en/function.mb-detect-encoding.php
      */
-    CONST DEFAULT_MB_DETECT_ORDER =
-        'UTF-8, ASCII, ISO-8859-1, ISO-8859-2, ISO-8859-3, ISO-8859-4, ISO-8859-5, ISO-8859-6, ' .
-        'ISO-8859-7, ISO-8859-8, ISO-8859-9, ISO-8859-10, ISO-8859-13, ISO-8859-14, ISO-8859-15, ' .
-        'ISO-8859-16, Windows-1251, Windows-1252, Windows-1254';
+    const DEFAULT_MB_DETECT_ORDER = 'UTF-8, ASCII, ISO-8859-1, ISO-8859-2, ISO-8859-3, ISO-8859-4, ISO-8859-5, ISO-8859-6, ISO-8859-7, ISO-8859-8, ISO-8859-9, ISO-8859-10, ISO-8859-13, ISO-8859-14, ISO-8859-15, ISO-8859-16, Windows-1251, Windows-1252, Windows-1254';
 
     /**
      * This is the default mb detect order for the detection of encoding
@@ -46,7 +43,8 @@ class Raven_Serializer
     /**
      * @param null|string $mb_detect_order
      */
-    public function __construct ($mb_detect_order = null) {
+    public function __construct ($mb_detect_order = null)
+    {
         if ($mb_detect_order != null) {
             $this->mb_detect_order = $mb_detect_order;
         }
