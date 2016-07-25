673b8775e4f72230cb55be5e416768daff7f9035
David Cramer <dcramer@gmail.com>
2016-07-25 13:44:21 -0700

    Expand example app

--- examples/vanilla/index.php
+++ examples/vanilla/index.php
@@ -2,21 +2,24 @@
 
 error_reporting(E_ALL);
 
+define('SENTRY_DSN', 'https://e9ebbd88548a441288393c457ec90441:399aaee02d454e2ca91351f29bdc3a07@app.getsentry.com/3235');
+
 require_once '../../lib/Raven/Autoloader.php';
 Raven_Autoloader::register();
 
-
 function setupSentry()
 {
-    $client = new \Raven_Client('https://e9ebbd88548a441288393c457ec90441:399aaee02d454e2ca91351f29bdc3a07@app.getsentry.com/3235');
-    $error_handler = new Raven_ErrorHandler($client);
-    $error_handler->registerExceptionHandler();
-    $error_handler->registerErrorHandler();
-    $error_handler->registerShutdownFunction();
+    (new \Raven_Client(SENTRY_DSN))
+        ->setAppPath(__DIR__)
+        ->setRelease(Raven_Client::VERSION)
+        ->setPrefixes(array(__DIR__))
+        ->install();
 }
 
 function createError()
 {
+    echo($undefined['foobar']);
+
     1 / 0;
 }
 
