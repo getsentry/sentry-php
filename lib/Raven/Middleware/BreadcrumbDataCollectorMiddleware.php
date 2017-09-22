9890322ac9f7547c7b9a7596ab48ca5c93714b34
Stefano Arlandini <sarlandini@alice.it>
2017-09-23 01:20:29 +0200

    Add a middleware that collects the breadcrumbs

---
+++ lib/Raven/Middleware/BreadcrumbDataCollectorMiddleware.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of Raven.
+ *
+ * (c) Sentry Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Raven\Middleware;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Raven\Breadcrumbs\Recorder;
+use Raven\Event;
+
+/**
+ * This middleware collects all the recorded breadcrumbs up to this moment and
+ * adds them to the event.
+ *
+ * @author Stefano Arlandini <sarlandini@alice.it>
+ */
+final class BreadcrumbDataCollectorMiddleware
+{
+    /**
+     * @var Recorder The breadcrumbs recorder
+     */
+    private $recorder;
+
+    /**
+     * Constructor.
+     *
+     * @param Recorder $recorder The breadcrumbs recorder
+     */
+    public function __construct(Recorder $recorder)
+    {
+        $this->recorder = $recorder;
+    }
+
+    /**
+     * Collects the needed data and sets it in the given event object.
+     *
+     * @param Event                       $event     The event being processed
+     * @param callable                    $next      The next middleware to call
+     * @param ServerRequestInterface|null $request   The request, if available
+     * @param \Exception|null             $exception The thrown exception, if available
+     * @param array                       $payload   Additional data
+     *
+     * @return Event
+     */
+    public function __invoke(Event $event, callable $next, ServerRequestInterface $request = null, \Exception $exception = null, array $payload = [])
+    {
+        foreach ($this->recorder as $breadcrumb) {
+            $event = $event->withBreadcrumb($breadcrumb);
+        }
+
+        return $next($event, $request, $exception, $payload);
+    }
+}
---
+++ tests/Middleware/BreadcrumbDataCollectorMiddlewareTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of Raven.
+ *
+ * (c) Sentry Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Raven\Tests\Breadcrumbs;
+
+use Raven\Breadcrumbs\Breadcrumb;
+use Raven\Breadcrumbs\Recorder;
+use Raven\Client;
+use Raven\Configuration;
+use Raven\Event;
+use Raven\Middleware\BreadcrumbDataCollectorMiddleware;
+
+class BreadcrumbDataCollectorMiddlewareTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInvoke()
+    {
+        $breadcrumb = new Breadcrumb(Client::LEVEL_INFO, Breadcrumb::TYPE_USER, 'foo');
+        $breadcrumb2 = new Breadcrumb(Client::LEVEL_ERROR, Breadcrumb::TYPE_ERROR, 'bar');
+
+        $recorder = new Recorder();
+        $recorder->record($breadcrumb);
+        $recorder->record($breadcrumb2);
+
+        $configuration = new Configuration();
+        $event = new Event($configuration);
+
+        $invokationCount = 0;
+        $callback = function (Event $eventArg) use ($event, $breadcrumb, $breadcrumb2, &$invokationCount) {
+            $this->assertNotSame($event, $eventArg);
+            $this->assertEquals([$breadcrumb, $breadcrumb2], $eventArg->getBreadcrumbs());
+
+            ++$invokationCount;
+        };
+
+        $middleware = new BreadcrumbDataCollectorMiddleware($recorder);
+        $middleware($event, $callback);
+
+        $this->assertEquals(1, $invokationCount);
+    }
+}
