d2d418e0471294fb64dbcafe677e9c39daad69fa
Kirill Lyubaev <kalyubaev@avito.ru>
2017-09-27 16:56:56 +0300

    Added checker for _convertPath if the path is file

--- lib/Raven/Client.php
+++ lib/Raven/Client.php
@@ -285,8 +285,8 @@ class Raven_Client
         // we need app_path to have a trailing slash otherwise
         // base path detection becomes complex if the same
         // prefix is matched
-        if (substr($path, 0, 1) === DIRECTORY_SEPARATOR && substr($path, -1) !== DIRECTORY_SEPARATOR) {
-            $path = $path.DIRECTORY_SEPARATOR;
+        if ($path{0} === DIRECTORY_SEPARATOR && substr($path, -1) !== DIRECTORY_SEPARATOR && !is_file($path)) {
+            $path .= DIRECTORY_SEPARATOR;
         }
         return $path;
     }
--- test/Raven/Tests/ClientTest.php
+++ test/Raven/Tests/ClientTest.php
@@ -1561,6 +1561,9 @@ class Raven_Tests_ClientTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('foo/bar/', $property->invoke(null, 'foo/bar/'));
         $this->assertEquals(dirname(__DIR__).'/', $property->invoke(null, __DIR__.'/../'));
         $this->assertEquals(dirname(dirname(__DIR__)).'/', $property->invoke(null, __DIR__.'/../../'));
+
+        $path = stream_get_meta_data($_ = tmpfile())['uri'];
+        $this->assertEquals($path, $property->invoke(null, $path));
     }
 
     /**
