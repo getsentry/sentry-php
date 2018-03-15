67bf787b053c8e4463bf873b8161c1cee635e143
Stefano Arlandini <sarlandini@alice.it>
2018-03-15 15:41:10 +0100

    Add a specialized context class that stores the runtime information

---
+++ lib/Raven/Context/RuntimeContext.php
@@ -0,0 +1,123 @@
+<?php
+
+/*
+ * This file is part of Raven.
+ *
+ * (c) Sentry Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Raven\Context;
+
+use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
+use Symfony\Component\OptionsResolver\Exception\UndefinedOptionsException;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * This class is a specialized implementation of the {@see `Context`} class
+ * that stores information about the current runtime.
+ *
+ * @author Stefano Arlandini <sarlandini@alice.it>
+ */
+class RuntimeContext extends Context
+{
+    /**
+     * @var OptionsResolver The options resolver
+     */
+    private $resolver;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws UndefinedOptionsException If any of the options are not supported
+     *                                   by the context
+     * @throws InvalidOptionsException   If any of the options don't fulfill the
+     *                                   specified validation rules
+     */
+    public function __construct(array $data = [])
+    {
+        $this->resolver = new OptionsResolver();
+
+        $this->configureOptions($this->resolver);
+
+        parent::__construct($this->resolver->resolve($data));
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws UndefinedOptionsException If any of the options are not supported
+     *                                   by the context
+     * @throws InvalidOptionsException   If any of the options don't fulfill the
+     *                                   specified validation rules
+     */
+    public function merge(array $data, $recursive = false)
+    {
+        $data = $this->resolver->resolve($data);
+
+        parent::merge($data, $recursive);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws UndefinedOptionsException If any of the options are not supported
+     *                                   by the context
+     * @throws InvalidOptionsException   If any of the options don't fulfill the
+     *                                   specified validation rules
+     */
+    public function setData(array $data)
+    {
+        $data = $this->resolver->resolve($data);
+
+        parent::setData($data);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws UndefinedOptionsException If any of the options are not supported
+     *                                   by the context
+     * @throws InvalidOptionsException   If any of the options don't fulfill the
+     *                                   specified validation rules
+     */
+    public function replaceData(array $data)
+    {
+        $data = $this->resolver->resolve($data);
+
+        parent::replaceData($data);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws UndefinedOptionsException If any of the options are not supported
+     *                                   by the context
+     * @throws InvalidOptionsException   If any of the options don't fulfill the
+     *                                   specified validation rules
+     */
+    public function offsetSet($offset, $value)
+    {
+        $data = $this->resolver->resolve([$offset => $value]);
+
+        parent::offsetSet($offset, $data[$offset]);
+    }
+
+    /**
+     * Configures the options of the context.
+     *
+     * @param OptionsResolver $resolver The resolver for the options
+     */
+    private function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'name' => 'php',
+            'version' => PHP_VERSION,
+        ]);
+
+        $resolver->setAllowedTypes('name', 'string');
+        $resolver->setAllowedTypes('version', 'string');
+    }
+}
---
+++ tests/Context/RuntimeContextTest.php
@@ -0,0 +1,191 @@
+<?php
+
+/*
+ * This file is part of Raven.
+ *
+ * (c) Sentry Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Raven\Tests\Context;
+
+use PHPUnit\Framework\TestCase;
+use Raven\Context\RuntimeContext;
+use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
+use Symfony\Component\OptionsResolver\Exception\UndefinedOptionsException;
+
+class RuntimeContextTest extends TestCase
+{
+    /**
+     * @dataProvider valuesDataProvider
+     */
+    public function testConstructor($initialData, $expectedData, $expectedExceptionClass, $expectedExceptionMessage)
+    {
+        if (null !== $expectedExceptionClass) {
+            $this->expectException($expectedExceptionClass);
+            $this->expectExceptionMessage($expectedExceptionMessage);
+        }
+
+        $context = new RuntimeContext($initialData);
+
+        $this->assertEquals($expectedData, $context->toArray());
+    }
+
+    /**
+     * @dataProvider valuesDataProvider
+     */
+    public function testMerge($initialData, $expectedData, $expectedExceptionClass, $expectedExceptionMessage)
+    {
+        if (null !== $expectedExceptionClass) {
+            $this->expectException($expectedExceptionClass);
+            $this->expectExceptionMessage($expectedExceptionMessage);
+        }
+
+        $context = new RuntimeContext();
+        $context->merge($initialData);
+
+        $this->assertEquals($expectedData, $context->toArray());
+    }
+
+    /**
+     * @dataProvider valuesDataProvider
+     */
+    public function testSetData($initialData, $expectedData, $expectedExceptionClass, $expectedExceptionMessage)
+    {
+        if (null !== $expectedExceptionClass) {
+            $this->expectException($expectedExceptionClass);
+            $this->expectExceptionMessage($expectedExceptionMessage);
+        }
+
+        $context = new RuntimeContext();
+        $context->setData($initialData);
+
+        $this->assertEquals($expectedData, $context->toArray());
+    }
+
+    /**
+     * @dataProvider valuesDataProvider
+     */
+    public function testReplaceData($initialData, $expectedData, $expectedExceptionClass, $expectedExceptionMessage)
+    {
+        if (null !== $expectedExceptionClass) {
+            $this->expectException($expectedExceptionClass);
+            $this->expectExceptionMessage($expectedExceptionMessage);
+        }
+
+        $context = new RuntimeContext();
+        $context->replaceData($initialData);
+
+        $this->assertEquals($expectedData, $context->toArray());
+    }
+
+    public function valuesDataProvider()
+    {
+        return [
+            [
+                [],
+                [
+                    'name' => 'php',
+                    'version' => PHP_VERSION,
+                ],
+                null,
+                null,
+            ],
+            [
+                [
+                    'name' => 'foo',
+                ],
+                [
+                    'name' => 'foo',
+                    'version' => PHP_VERSION,
+                ],
+                null,
+                null,
+            ],
+            [
+                [
+                    'name' => 'foo',
+                    'version' => 'bar',
+                ],
+                [
+                    'name' => 'foo',
+                    'version' => 'bar',
+                ],
+                null,
+                null,
+            ],
+            [
+                [
+                    'foo' => 'bar',
+                ],
+                [],
+                UndefinedOptionsException::class,
+                'The option "foo" does not exist. Defined options are: "name", "version".',
+            ],
+            [
+                [
+                    'name' => 1,
+                ],
+                [],
+                InvalidOptionsException::class,
+                'The option "name" with value 1 is expected to be of type "string", but is of type "integer".',
+            ],
+            [
+                [
+                    'version' => 1,
+                ],
+                [],
+                InvalidOptionsException::class,
+                'The option "version" with value 1 is expected to be of type "string", but is of type "integer".',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider offsetSetDataProvider
+     */
+    public function testOffsetSet($key, $value, $expectedExceptionClass, $expectedExceptionMessage)
+    {
+        if (null !== $expectedExceptionClass) {
+            $this->expectException($expectedExceptionClass);
+            $this->expectExceptionMessage($expectedExceptionMessage);
+        }
+
+        $context = new RuntimeContext();
+        $context[$key] = $value;
+
+        $this->assertArraySubset([$key => $value], $context->toArray());
+    }
+
+    public function offsetSetDataProvider()
+    {
+        return [
+            [
+                'name',
+                'foo',
+                null,
+                null,
+            ],
+            [
+                'name',
+                1,
+                InvalidOptionsException::class,
+                'The option "name" with value 1 is expected to be of type "string", but is of type "integer".',
+            ],
+            [
+                'version',
+                1,
+                InvalidOptionsException::class,
+                'The option "version" with value 1 is expected to be of type "string", but is of type "integer".',
+            ],
+            [
+                'foo',
+                'bar',
+                UndefinedOptionsException::class,
+                'The option "foo" does not exist. Defined options are: "name", "version".',
+            ],
+        ];
+    }
+}
