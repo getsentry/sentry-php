8dc3f417cc2ff8c43d0ccfb82432db68752fb518
Stefano Arlandini <ste93cry@users.noreply.github.com>
2018-03-30 01:09:26 +0200

    Support logging of Composer packages (#577)

---
+++ tests/Middleware/ModulesMiddlewareTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of Raven.
+ *
+ * (c) Sentry Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Raven\Tests\Breadcrumbs;
+
+use PHPUnit\Framework\TestCase;
+use Raven\Configuration;
+use Raven\Event;
+use Raven\Middleware\ModulesMiddleware;
+
+class ModulesMiddlewareTest extends TestCase
+{
+    public function testInvoke()
+    {
+        $configuration = new Configuration(['project_root' => __DIR__ . '/../Fixtures']);
+        $event = new Event($configuration);
+
+        $callbackInvoked = false;
+        $callback = function (Event $eventArg) use ($event, &$callbackInvoked) {
+            $this->assertNotSame($event, $eventArg);
+            $this->assertEquals(['foo/bar' => '1.2.3.0', 'foo/baz' => '4.5.6.0'], $eventArg->getModules());
+
+            $callbackInvoked = true;
+        };
+
+        $middleware = new ModulesMiddleware($configuration);
+        $middleware($event, $callback);
+
+        $this->assertTrue($callbackInvoked);
+    }
+
+    public function testInvokeDoesNothingWhenNoComposerLockFileExists()
+    {
+        $configuration = new Configuration(['project_root' => __DIR__]);
+        $event = new Event($configuration);
+
+        $callbackInvoked = false;
+        $callback = function (Event $eventArg) use ($event, &$callbackInvoked) {
+            $this->assertSame($event, $eventArg);
+
+            $callbackInvoked = true;
+        };
+
+        $middleware = new ModulesMiddleware($configuration);
+        $middleware($event, $callback);
+
+        $this->assertTrue($callbackInvoked);
+    }
+}
