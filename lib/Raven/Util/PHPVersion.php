9c5c0b4ad917fd59f1088f9263f93ad2ee08b407
Alessandro Lai <alessandro.lai85@gmail.com>
2018-08-03 17:04:59 +0200

    Implement PHPVersion parsing util class (ported from 763f675)

---
+++ lib/Raven/Util/PHPVersion.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Raven\Util;
+
+class PHPVersion
+{
+    const VERSION_PARSING_REGEX = '/^(?<base>\d\.\d\.\d{1,2})(?<extra>-(beta|rc)-?(\d+)?(-dev)?)?/i';
+
+    /**
+     * @param string $version
+     * @return string
+     */
+    public static function getParsed($version = PHP_VERSION)
+    {
+        if (! preg_match(self::VERSION_PARSING_REGEX, $version, $matches)) {
+            return $version;
+        }
+
+        $version = $matches['base'];
+        if (isset($matches['extra'])) {
+            $version .=  $matches['extra'];
+        }
+
+        return $version;
+    }
+}
