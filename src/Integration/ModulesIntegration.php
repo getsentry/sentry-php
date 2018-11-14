7ce9c411f912e5d1e8237bdc0cb27e680bcadcb9
Alessandro Lai <alessandro.lai85@gmail.com>
2018-11-14 15:20:40 +0100

    Refactor ModulesIntegration to use jean85/pretty-package-versions

--- src/Integration/ModulesIntegration.php
+++ src/Integration/ModulesIntegration.php
@@ -5,10 +5,9 @@ declare(strict_types=1);
 namespace Sentry\Integration;
 
 use Composer\Composer;
-use Composer\Factory;
-use Composer\IO\NullIO;
+use Jean85\PrettyVersions;
+use PackageVersions\Versions;
 use Sentry\Event;
-use Sentry\Options;
 use Sentry\State\Hub;
 use Sentry\State\Scope;
 
@@ -21,27 +20,15 @@ use Sentry\State\Scope;
 final class ModulesIntegration implements IntegrationInterface
 {
     /**
-     * @var Options The client option
-     */
-    private $options;
-
-    /**
      * @var array The list of installed vendors
      */
     private static $loadedModules = [];
 
-    /**
-     * Constructor.
-     *
-     * @param Options $options The Client options
-     */
-    public function __construct(Options $options)
+    public function __construct()
     {
-        if (!class_exists(Composer::class)) {
-            throw new \LogicException('You need the "composer/composer" package in order to use this integration.');
+        if (!class_exists(PrettyVersions::class)) {
+            throw new \LogicException('You need the "jean85/pretty-package-versions" package in order to use this integration.');
         }
-
-        $this->options = $options;
     }
 
     /**
@@ -61,21 +48,14 @@ final class ModulesIntegration implements IntegrationInterface
     }
 
     /**
-     * @param ModulesIntegration $self  The instance of this integration
-     * @param Event              $event The event that will be enriched with the modules
+     * @param self  $self  The instance of this integration
+     * @param Event $event The event that will be enriched with the modules
      */
     public static function applyToEvent(self $self, Event $event): void
     {
-        $composerFilePath = $self->options->getProjectRoot() . \DIRECTORY_SEPARATOR . 'composer.json';
-
-        if (file_exists($composerFilePath) && 0 == \count(self::$loadedModules)) {
-            $composer = Factory::create(new NullIO(), $composerFilePath, true);
-            $locker = $composer->getLocker();
-
-            if ($locker->isLocked()) {
-                foreach ($locker->getLockedRepository()->getPackages() as $package) {
-                    self::$loadedModules[$package->getName()] = $package->getVersion();
-                }
+        if (empty(self::$loadedModules)) {
+            foreach (Versions::VERSIONS as $package => $rawVersion) {
+                self::$loadedModules[$package] = PrettyVersions::getVersion($package)->getPrettyVersion();
             }
         }
 
