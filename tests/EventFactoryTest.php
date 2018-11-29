a562dae3635f035e00f8419620dd7cb634cbbc2a
Alessandro Lai <alessandro.lai85@gmail.com>
2018-11-30 00:20:53 +0100

    Add EventFactoryTest

---
+++ tests/EventFactoryTest.php
@@ -0,0 +1,175 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sentry\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Sentry\EventFactory;
+use Sentry\Frame;
+use Sentry\Options;
+use Sentry\Serializer\RepresentationSerializerInterface;
+use Sentry\Serializer\Serializer;
+use Sentry\Serializer\SerializerInterface;
+use Sentry\Severity;
+use Sentry\Stacktrace;
+
+class EventFactoryTest extends TestCase
+{
+    public function testCreateEventWithDefaultValues(): void
+    {
+        $options = new Options();
+        $options->setServerName('testServerName');
+        $options->setRelease('testRelease');
+        $options->setTags(['test' => 'tag']);
+        $options->setEnvironment('testEnvironment');
+
+        $_SERVER['PATH_INFO'] = 'testPathInfo';
+
+        $eventFactory = new EventFactory(
+            $this->createMock(SerializerInterface::class),
+            $this->createMock(RepresentationSerializerInterface::class),
+            $options,
+            'sentry.sdk.identifier'
+        );
+
+        $event = $eventFactory->create([]);
+
+        $this->assertSame($options->getServerName(), $event->getServerName());
+        $this->assertSame($options->getRelease(), $event->getRelease());
+        $this->assertSame($options->getTags(), $event->getTagsContext()->toArray());
+        $this->assertSame($options->getEnvironment(), $event->getEnvironment());
+        $this->assertSame('testPathInfo', $event->getTransaction());
+        $this->assertNull($event->getStacktrace());
+    }
+
+    /**
+     * @dataProvider createWithPayloadDataProvider
+     */
+    public function testCreateWithPayload(array $payload, array $expectedSubset): void
+    {
+        $eventFactory = new EventFactory(
+            $this->createMock(SerializerInterface::class),
+            $this->createMock(RepresentationSerializerInterface::class),
+            new Options(),
+            'sentry.sdk.identifier'
+        );
+
+        $event = $eventFactory->create($payload);
+
+        $this->assertArraySubset($expectedSubset, $event->toArray());
+    }
+
+    public function createWithPayloadDataProvider()
+    {
+        return [
+            [
+                ['transaction' => 'testTransaction'],
+                ['transaction' => 'testTransaction'],
+            ],
+            [
+                ['logger' => 'testLogger'],
+                ['logger' => 'testLogger'],
+            ],
+            [
+                ['message' => 'testMessage'],
+                ['message' => 'testMessage'],
+            ],
+            [
+                [
+                    'message' => 'testMessage %s',
+                    'message_params' => ['param'],
+                ],
+                [
+                    'message' => [
+                        'message' => 'testMessage %s',
+                        'params' => ['param'],
+                        'formatted' => 'testMessage param',
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function testCreateEventInCLIDoesntSetTransaction(): void
+    {
+        $eventFactory = new EventFactory(
+            $this->createMock(SerializerInterface::class),
+            $this->createMock(RepresentationSerializerInterface::class),
+            new Options(),
+            'sentry.sdk.identifier'
+        );
+
+        $event = $eventFactory->create([]);
+
+        $this->assertNull($event->getTransaction());
+    }
+
+    public function testCreateWithException(): void
+    {
+        $previousException = new \RuntimeException('testMessage2');
+        $exception = new \Exception('testMessage', 0, $previousException);
+
+        $eventFactory = new EventFactory(
+            new Serializer(),
+            $this->createMock(RepresentationSerializerInterface::class),
+            new Options(),
+            'sentry.sdk.identifier'
+        );
+
+        $event = $eventFactory->create(['exception' => $exception]);
+
+        $expectedData = [
+            [
+                'type' => \Exception::class,
+                'value' => 'testMessage',
+            ],
+            [
+                'type' => \RuntimeException::class,
+                'value' => 'testMessage2',
+            ],
+        ];
+        $this->assertArraySubset($expectedData, $event->getExceptions());
+
+        foreach ($event->getExceptions() as $exceptionData) {
+            $this->assertInstanceOf(Stacktrace::class, $exceptionData['stacktrace']);
+        }
+    }
+
+    public function testCreateWithErrorException(): void
+    {
+        $exception = new \ErrorException('testMessage', 0, E_USER_ERROR);
+
+        $eventFactory = new EventFactory(
+            new Serializer(),
+            $this->createMock(RepresentationSerializerInterface::class),
+            new Options(),
+            'sentry.sdk.identifier'
+        );
+
+        $event = $eventFactory->create(['exception' => $exception]);
+
+        $this->assertTrue(Severity::error()->isEqualTo($event->getLevel()));
+    }
+
+    public function testCreateWithAutoStacktrace(): void
+    {
+        $options = new Options();
+        $options->setAttachStacktrace(true);
+
+        $eventFactory = new EventFactory(
+            $this->createMock(SerializerInterface::class),
+            $this->createMock(RepresentationSerializerInterface::class),
+            $options,
+            'sentry.sdk.identifier'
+        );
+
+        $event = $eventFactory->create([]);
+
+        $stacktrace = $event->getStacktrace();
+        $this->assertInstanceOf(Stacktrace::class, $stacktrace);
+        /** @var Frame $lastFrame */
+        $lastFrame = array_reverse($stacktrace->getFrames())[0];
+        $this->assertSame('src/EventFactory.php', $lastFrame->getFile());
+    }
+}
