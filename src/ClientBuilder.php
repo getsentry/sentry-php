35e86a6c113124f768cfa4a4c6ffa0e6e2b94916
HazA <daniel.griesser.86@gmail.com>
2019-02-22 14:25:18 +0100

    ref: Rename CurlClient to HttpCurlClient

--- src/ClientBuilder.php
+++ src/ClientBuilder.php
@@ -13,7 +13,7 @@ use Http\Client\Common\Plugin\HeaderSetPlugin;
 use Http\Client\Common\Plugin\RetryPlugin;
 use Http\Client\Common\PluginClient;
 use Http\Client\HttpAsyncClient;
-use Http\Client\Curl\Client as CurlClient;
+use Http\Client\Curl\Client as HttpCurlClient;
 use Http\Discovery\ClassDiscovery;
 use Http\Discovery\HttpAsyncClientDiscovery;
 use Http\Discovery\MessageFactoryDiscovery;
@@ -318,8 +318,8 @@ final class ClientBuilder implements ClientBuilderInterface
         $this->uriFactory = $this->uriFactory ?? UriFactoryDiscovery::find();
 
         if (null === $this->httpClient && null !== $this->options->getHttpProxy()) {
-            if (ClassDiscovery::safeClassExists(CurlClient::class)) {
-                $this->httpClient = new CurlClient(null, null, [
+            if (ClassDiscovery::safeClassExists(HttpCurlClient::class)) {
+                $this->httpClient = new HttpCurlClient(null, null, [
                     CURLOPT_PROXY => $this->options->getHttpProxy(),
                 ]);
             } else {
