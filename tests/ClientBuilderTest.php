864e28615c1eb0e442fec9e57a8c8279a3346a0b
Alessandro Lai <alessandro.lai85@gmail.com>
2019-02-22 22:51:35 +0100

    Add test

--- tests/ClientBuilderTest.php
+++ tests/ClientBuilderTest.php
@@ -290,6 +290,22 @@ final class ClientBuilderTest extends TestCase
             ClientBuilder::create([])
         );
     }
+
+    public function testCreateWithHttpProxyAndCustomTransportThrowsException(): void
+    {
+        $options = new Options([
+            'dsn' => 'http://public:secret@example.com/sentry/1',
+            'http_proxy' => 'some-proxy',
+        ]);
+
+        $clientBuilder = new ClientBuilder($options);
+        $clientBuilder->setHttpClient($this->createMock(HttpAsyncClient::class));
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('http_proxy');
+
+        $clientBuilder->getClient();
+    }
 }
 
 final class StubIntegration implements IntegrationInterface
