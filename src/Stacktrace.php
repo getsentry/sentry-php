23543bc3a6d5728617adb51b632933dc8ac90efd
Chris Kruining <chris@kruining.eu>
2019-05-27 16:05:48 +0200

    Fix exception, open_basedir restriction in effect
    this is a *VERY* crude fix for this exception `is_file(): open_basedir restriction in effect. File([internal]) is not within the allowed path(s)`

--- src/Stacktrace.php
+++ src/Stacktrace.php
@@ -224,7 +224,7 @@ class Stacktrace implements \JsonSerializable
      */
     protected function getSourceCodeExcerpt(string $path, int $lineNumber, int $maxLinesToFetch): array
     {
-        if (!is_file($path) || !is_readable($path)) {
+        if ($path === '[internal]' | !is_file($path) || !is_readable($path)) {
             return [];
         }
 
