9c91201730901b9d2bf2f937c766be18107de61f
Stefano Arlandini <sarlandini@alice.it>
2020-04-07 18:55:25 +0200

    Undeprecate the ExceptionInterface interface

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -8,8 +8,6 @@ namespace Sentry\Exception;
  * This interface must be implemented by all exception classes of this library.
  *
  * @author Stefano Arlandini <sarlandini@alice.it>
- *
- * @deprecated since version 2.4, to be removed in 3.0
  */
 interface ExceptionInterface
 {
