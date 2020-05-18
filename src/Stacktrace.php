getsentry/sentry-php

commit 97bbca2c828651dfbd04769c94c1cc8b37e096a3

Fix the behavior of the context_lines option as per documentation when null is used (#1003)

src/Stacktrace.php
@@ -214,13 +214,13 @@ class Stacktrace implements \JsonSerializable
     /**
      * Gets an excerpt of the source code around a given line.
      *
-     * @param string $path            The file path
-     * @param int    $lineNumber      The line to centre about
-     * @param int    $maxLinesToFetch The maximum number of lines to fetch
+     * @param string   $path            The file path
+     * @param int      $lineNumber      The line to centre about
+     * @param int|null $maxLinesToFetch The maximum number of lines to fetch
      */
-    protected function getSourceCodeExcerpt(string $path, int $lineNumber, int $maxLinesToFetch): array
+    protected function getSourceCodeExcerpt(string $path, int $lineNumber, ?int $maxLinesToFetch): array
     {
-        if (@!is_readable($path) || !is_file($path)) {
+        if (null === $maxLinesToFetch || @!is_readable($path) || !is_file($path)) {
             return [];
         }
 

src/Options.php
@@ -163,7 +163,7 @@ final class Options
     /**
      * Gets the number of lines of code context to capture, or null if none.
      */
-    public function getContextLines(): int
+    public function getContextLines(): ?int
     {
         return $this->options['context_lines'];
     }
@@ -171,9 +171,9 @@ final class Options
     /**
      * Sets the number of lines of code context to capture, or null if none.
      *
-     * @param int $contextLines The number of lines of code
+     * @param int|null $contextLines The number of lines of code
      */
-    public function setContextLines(int $contextLines): void
+    public function setContextLines(?int $contextLines): void
     {
         $options = array_merge($this->options, ['context_lines' => $contextLines]);
 
@@ -804,7 +804,7 @@ final class Options
         $resolver->setAllowedTypes('prefixes', 'array');
         $resolver->setAllowedTypes('sample_rate', ['int', 'float']);
         $resolver->setAllowedTypes('attach_stacktrace', 'bool');
-        $resolver->setAllowedTypes('context_lines', 'int');
+        $resolver->setAllowedTypes('context_lines', ['null', 'int']);
         $resolver->setAllowedTypes('enable_compression', 'bool');
         $resolver->setAllowedTypes('environment', ['null', 'string']);
         $resolver->setAllowedTypes('excluded_exceptions', 'array');
@@ -835,6 +835,7 @@ final class Options
         $resolver->setAllowedValues('max_breadcrumbs', \Closure::fromCallable([$this, 'validateMaxBreadcrumbsOptions']));
         $resolver->setAllowedValues('class_serializers', \Closure::fromCallable([$this, 'validateClassSerializersOption']));
         $resolver->setAllowedValues('tags', \Closure::fromCallable([$this, 'validateTagsOption']));
+        $resolver->setAllowedValues('context_lines', \Closure::fromCallable([$this, 'validateContextLinesOption']));
 
         $resolver->setNormalizer('dsn', \Closure::fromCallable([$this, 'normalizeDsnOption']));
         $resolver->setNormalizer('project_root', function (SymfonyOptions $options, ?string $value) {
@@ -1035,6 +1036,16 @@ final class Options
         }
 
         return true;
+    }
+
+    /**
+     * Validates that the value passed to the "context_lines" option is valid.
+     *
+     * @param int|null $contextLines The value to validate
+     */
+    private function validateContextLinesOption(?int $contextLines): bool
+    {
+        return null === $contextLines || $contextLines >= 0;
     }
 
     /**

src/Serializer/RepresentationSerializer.php
@@ -12,6 +12,9 @@ class RepresentationSerializer extends AbstractSerializer implements Representat
 {
     /**
      * {@inheritdoc}
+     *
+     * @psalm-suppress InvalidReturnType
+     * @psalm-suppress InvalidReturnStatement
      */
     public function representationSerialize($value)
     {

tests/Integration/ModulesIntegrationTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Sentry\Tests\Integration;
 
-use Jean85\PrettyVersions;
 use PHPUnit\Framework\TestCase;
 use Sentry\Event;
 use Sentry\Integration\ModulesIntegration;
@@ -20,8 +19,6 @@ final class ModulesIntegrationTest extends TestCase
 
         $modules = $event->getModules();
 
-        $this->assertArrayHasKey('sentry/sentry', $modules, 'Root project missing');
-        $this->assertArrayHasKey('ocramius/package-versions', $modules, 'Indirect dependency missing');
-        $this->assertEquals(PrettyVersions::getVersion('sentry/sentry'), $modules['sentry/sentry']);
+        $this->assertNotEmpty($modules);
     }
 }

tests/OptionsTest.php
@@ -305,6 +305,44 @@ final class OptionsTest extends TestCase
         ];
     }
 
+    /**
+     * @dataProvider contextLinesOptionValidatesInputValueDataProvider
+     */
+    public function testContextLinesOptionValidatesInputValue(?int $value, ?string $expectedExceptionMessage): void
+    {
+        if (null !== $expectedExceptionMessage) {
+            $this->expectException(InvalidOptionsException::class);
+            $this->expectExceptionMessage($expectedExceptionMessage);
+        } else {
+            $this->expectNotToPerformAssertions();
+        }
+
+        new Options(['context_lines' => $value]);
+    }
+
+    public function contextLinesOptionValidatesInputValueDataProvider(): \Generator
+    {
+        yield [
+            -1,
+            'The option "context_lines" with value -1 is invalid.',
+        ];
+
+        yield [
+            0,
+            null,
+        ];
+
+        yield [
+            1,
+            null,
+        ];
+
+        yield [
+            null,
+            null,
+        ];
+    }
+
     public function testDsnOptionSupportsEnvironmentVariable(): void
     {
         $_SERVER['SENTRY_DSN'] = 'http://public@example.com/1';

tests/StacktraceTest.php
@@ -300,9 +300,7 @@ final class StacktraceTest extends TestCase
      */
     public function testAddFrameRespectsContextLinesOption(string $fixture, int $lineNumber, ?int $contextLines, int $preContextCount, int $postContextCount): void
     {
-        if (null !== $contextLines) {
-            $this->options->setContextLines($contextLines);
-        }
+        $this->options->setContextLines($contextLines);
 
         $fileContent = explode("\n", $this->getFixture($fixture));
         $stacktrace = new Stacktrace($this->options, $this->serializer, $this->representationSerializer);
@@ -316,25 +314,68 @@ final class StacktraceTest extends TestCase
         $this->assertCount($postContextCount, $frames[0]->getPostContext());
 
         for ($i = 0; $i < $preContextCount; ++$i) {
-            $this->assertEquals(rtrim($fileContent[$i + ($lineNumber - $preContextCount - 1)]), $frames[0]->getPreContext()[$i]);
+            $this->assertSame(rtrim($fileContent[$i + ($lineNumber - $preContextCount - 1)]), $frames[0]->getPreContext()[$i]);
         }
 
-        $this->assertEquals(rtrim($fileContent[$lineNumber - 1]), $frames[0]->getContextLine());
+        if (null === $contextLines) {
+            $this->assertNull($frames[0]->getContextLine());
+        } else {
+            $this->assertSame(rtrim($fileContent[$lineNumber - 1]), $frames[0]->getContextLine());
+        }
 
         for ($i = 0; $i < $postContextCount; ++$i) {
-            $this->assertEquals(rtrim($fileContent[$i + $lineNumber]), $frames[0]->getPostContext()[$i]);
+            $this->assertSame(rtrim($fileContent[$i + $lineNumber]), $frames[0]->getPostContext()[$i]);
         }
     }
 
-    public function addFrameRespectsContextLinesOptionDataProvider(): array
+    public function addFrameRespectsContextLinesOptionDataProvider(): \Generator
     {
-        return [
-            'read code from short file' => ['code/ShortFile.php', 3, 2, 2, 2],
-            'read code from long file with default context' => ['code/LongFile.php', 8, null, 5, 5],
-            'read code from long file with specified context' => ['code/LongFile.php', 8, 2, 2, 2],
-            'read code from short file with no context' => ['code/ShortFile.php', 3, 0, 0, 0],
-            'read code from long file near end of file' => ['code/LongFile.php', 11, 5, 5, 2],
-            'read code from long file near beginning of file' => ['code/LongFile.php', 3, 5, 2, 5],
+        yield 'skip reading code when context_lines option == 0' => [
+            'code/ShortFile.php',
+            3,
+            0,
+            0,
+            0,
+        ];
+
+        yield 'skip reading pre_context and post_context when context_lines == null' => [
+            'code/ShortFile.php',
+            3,
+            null,
+            0,
+            0,
+        ];
+
+        yield 'read code from short file' => [
+            'code/ShortFile.php',
+            3,
+            2,
+            2,
+            2,
+        ];
+
+        yield 'read code from long file' => [
+            'code/LongFile.php',
+            8,
+            2,
+            2,
+            2,
+        ];
+
+        yield 'read code from long file near end of file' => [
+            'code/LongFile.php',
+            11,
+            5,
+            5,
+            2,
+        ];
+
+        yield 'read code from long file near beginning of file' => [
+            'code/LongFile.php',
+            3,
+            5,
+            2,
+            5,
         ];
     }
 

commit 9309740c5a10540158e128549b8fc7b14a977baf

Fix BC break introduced with #1003 (#1018)

src/Stacktrace.php
@@ -141,18 +141,21 @@ class Stacktrace implements \JsonSerializable
         }
 
         $frame = new Frame($functionName, $this->stripPrefixFromFilePath($file), $line);
-        $sourceCodeExcerpt = $this->getSourceCodeExcerpt($file, $line, $this->options->getContextLines());
 
-        if (isset($sourceCodeExcerpt['pre_context'])) {
-            $frame->setPreContext($sourceCodeExcerpt['pre_context']);
-        }
+        if (null !== $this->options->getContextLines()) {
+            $sourceCodeExcerpt = $this->getSourceCodeExcerpt($file, $line, $this->options->getContextLines());
 
-        if (isset($sourceCodeExcerpt['context_line'])) {
-            $frame->setContextLine($sourceCodeExcerpt['context_line']);
-        }
+            if (isset($sourceCodeExcerpt['pre_context'])) {
+                $frame->setPreContext($sourceCodeExcerpt['pre_context']);
+            }
 
-        if (isset($sourceCodeExcerpt['post_context'])) {
-            $frame->setPostContext($sourceCodeExcerpt['post_context']);
+            if (isset($sourceCodeExcerpt['context_line'])) {
+                $frame->setContextLine($sourceCodeExcerpt['context_line']);
+            }
+
+            if (isset($sourceCodeExcerpt['post_context'])) {
+                $frame->setPostContext($sourceCodeExcerpt['post_context']);
+            }
         }
 
         $frame->setIsInApp($this->isFrameInApp($file, $functionName));
@@ -214,13 +217,13 @@ class Stacktrace implements \JsonSerializable
     /**
      * Gets an excerpt of the source code around a given line.
      *
-     * @param string   $path            The file path
-     * @param int      $lineNumber      The line to centre about
-     * @param int|null $maxLinesToFetch The maximum number of lines to fetch
+     * @param string $path            The file path
+     * @param int    $lineNumber      The line to centre about
+     * @param int    $maxLinesToFetch The maximum number of lines to fetch
      */
-    protected function getSourceCodeExcerpt(string $path, int $lineNumber, ?int $maxLinesToFetch): array
+    protected function getSourceCodeExcerpt(string $path, int $lineNumber, int $maxLinesToFetch): array
     {
-        if (null === $maxLinesToFetch || @!is_readable($path) || !is_file($path)) {
+        if (@!is_readable($path) || !is_file($path)) {
             return [];
         }
 