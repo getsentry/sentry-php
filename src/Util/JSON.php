a0832fe9284e509e8b15708463a7944cd6b73786
Viktor Szépe <viktor@szepe.net>
2020-08-23 01:08:06 +0000

    Fix object serialization

--- src/Util/JSON.php
+++ src/Util/JSON.php
@@ -93,7 +93,7 @@ final class JSON
         if (\is_array($data) || \is_object($data)) {
             $output = [];
 
-            foreach ((array) $data as $key => $value) {
+            foreach (\is_object($data) ? get_object_vars($data) : $data as $key => $value) {
                 if (\is_string($key)) {
                     $key = self::convertStringToUtf8($key);
                 }
