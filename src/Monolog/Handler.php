832363a41165325040e0f8871fffb1df31d99c1f
Viktor Szépe <viktor@szepe.net>
2020-08-23 02:27:21 +0200

    Fix by @psalm-suppress

--- src/Monolog/Handler.php
+++ src/Monolog/Handler.php
@@ -42,6 +42,7 @@ final class Handler extends AbstractProcessingHandler
     /**
      * {@inheritdoc}
      *
+     * @psalm-suppress MoreSpecificImplementedParamType
      * @param array{level:int, level_name:string, message:string, channel:string, context:mixed} $record
      */
     protected function write(array $record): void
