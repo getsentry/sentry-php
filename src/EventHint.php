937130d94a6f6c20249d6c301f412fd3ecc17cf9
Alex Bouma <me@alexbouma.me>
2020-09-23 16:20:16 +0200

    CS

--- src/EventHint.php
+++ src/EventHint.php
@@ -28,8 +28,6 @@ final class EventHint
     /**
      * Create a EventHint instance from an array of values.
      *
-     * @param array $hintData
-     *
      * @psalm-param array{
      *     exception?: \Throwable,
      *     stacktrace?: Event
