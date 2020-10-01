b84e11490f56054c468031ee190be7b08e0150e6
Alex Bouma <me@alexbouma.me>
2020-10-01 17:43:57 +0200

    Add one more test case

--- tests/State/HubTest.php
+++ tests/State/HubTest.php
@@ -515,6 +515,24 @@ final class HubTest extends TestCase
 
         yield [
             new Options([
+                'sample_rate' => 1.0,
+                'traces_sample_rate' => 0.0,
+            ]),
+            new TransactionContext(),
+            false,
+        ];
+
+        yield [
+            new Options([
+                'sample_rate' => 0.0,
+                'traces_sample_rate' => 1.0,
+            ]),
+            new TransactionContext(),
+            true,
+        ];
+
+        yield [
+            new Options([
                 'traces_sample_rate' => 0.5,
             ]),
             new TransactionContext(TransactionContext::DEFAULT_NAME, true),
@@ -527,15 +545,6 @@ final class HubTest extends TestCase
             ]),
             new TransactionContext(TransactionContext::DEFAULT_NAME, false),
             false,
-        ];
-
-        yield [
-            new Options([
-                'sample_rate' => 0.0,
-                'traces_sample_rate' => 1.0,
-            ]),
-            new TransactionContext(),
-            true,
         ];
     }
 
