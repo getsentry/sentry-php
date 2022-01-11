f2bec0876a19b268e998bf6565082c5a5650fcbc
Alessandro Lai <alessandro.lai85@gmail.com>
2022-01-11 10:29:18 +0100

    Add bench with injected context

--- tests/Benchmark/SpanBench.php
+++ tests/Benchmark/SpanBench.php
@@ -5,9 +5,18 @@ namespace Sentry\Tests\Benchmark;
 use PhpBench\Benchmark\Metadata\Annotations\Iterations;
 use PhpBench\Benchmark\Metadata\Annotations\Revs;
 use Sentry\Tracing\Span;
+use Sentry\Tracing\TransactionContext;
 
 class SpanBench
 {
+    /** @var TransactionContext */
+    private $context;
+
+    public function __construct()
+    {
+        $this->context = TransactionContext::fromSentryTrace('566e3688a61d4bc888951642d6f14a19-566e3688a61d4bc8-0');
+    }
+
     /**
      * @Revs(100000)
      * @Iterations(10)
@@ -15,5 +24,14 @@ class SpanBench
     public function benchConstructor(): void
     {
         $span = new Span();
+    }
+
+    /**
+     * @Revs(100000)
+     * @Iterations(10)
+     */
+    public function benchConstructorWithInjectedContext(): void
+    {
+        $span = new Span($this->context);
     }
 }
