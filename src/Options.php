fe10bc89e9da7e7aea5d40038bbd7bd1d765a76d
Alex Bouma <alex@bouma.me>
2022-05-11 15:38:07 +0200

    Lower default `send_attempts` option to `0`

--- src/Options.php
+++ src/Options.php
@@ -699,7 +699,7 @@ final class Options
         $resolver->setDefaults([
             'integrations' => [],
             'default_integrations' => true,
-            'send_attempts' => 3,
+            'send_attempts' => 0,
             'prefixes' => array_filter(explode(\PATH_SEPARATOR, get_include_path() ?: '')),
             'sample_rate' => 1,
             'traces_sample_rate' => 0,
