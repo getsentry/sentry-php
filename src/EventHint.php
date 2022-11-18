getsentry/sentry-php

commit 04d4280cd3e94ce7ff902dc0ae925f3607f11095

ref: Expose the ExceptionMechanism through the event hint (#1416)

src/EventHint.php
@@ -17,6 +17,13 @@ final class EventHint
     public $exception;
 
     /**
+     * An object describing the mechanism of the original exception.
+     *
+     * @var ExceptionMechanism|null
+     */
+    public $mechanism;
+
+    /**
      * The stacktrace to set on the event.
      *
      * @var Stacktrace|null
@@ -43,11 +50,16 @@ final class EventHint
     {
         $hint = new self();
         $exception = $hintData['exception'] ?? null;
+        $mechanism = $hintData['mechanism'] ?? null;
         $stacktrace = $hintData['stacktrace'] ?? null;
         $extra = $hintData['extra'] ?? [];
 
         if (null !== $exception && !$exception instanceof \Throwable) {
             throw new \InvalidArgumentException(sprintf('The value of the "exception" field must be an instance of a class implementing the "%s" interface. Got: "%s".', \Throwable::class, get_debug_type($exception)));
+        }
+
+        if (null !== $mechanism && !$mechanism instanceof ExceptionMechanism) {
+            throw new \InvalidArgumentException(sprintf('The value of the "mechanism" field must be an instance of the "%s" class. Got: "%s".', ExceptionMechanism::class, get_debug_type($mechanism)));
         }
 
         if (null !== $stacktrace && !$stacktrace instanceof Stacktrace) {
@@ -59,6 +71,7 @@ final class EventHint
         }
 
         $hint->exception = $exception;
+        $hint->mechanism = $mechanism;
         $hint->stacktrace = $stacktrace;
         $hint->extra = $extra;
 

src/Client.php
@@ -248,7 +248,7 @@ final class Client implements ClientInterface
     {
         if (null !== $hint) {
             if (null !== $hint->exception && empty($event->getExceptions())) {
-                $this->addThrowableToEvent($event, $hint->exception);
+                $this->addThrowableToEvent($event, $hint->exception, $hint);
             }
 
             if (null !== $hint->stacktrace && null === $event->getStacktrace()) {
@@ -338,8 +338,9 @@ final class Client implements ClientInterface
      *
      * @param Event      $event     The event that will be enriched with the exception
      * @param \Throwable $exception The exception that will be processed and added to the event
+     * @param EventHint  $hint      Contains additional information about the event
      */
-    private function addThrowableToEvent(Event $event, \Throwable $exception): void
+    private function addThrowableToEvent(Event $event, \Throwable $exception, EventHint $hint): void
     {
         if ($exception instanceof \ErrorException && null === $event->getLevel()) {
             $event->setLevel(Severity::fromError($exception->getSeverity()));
@@ -351,7 +352,7 @@ final class Client implements ClientInterface
             $exceptions[] = new ExceptionDataBag(
                 $exception,
                 $this->stacktraceBuilder->buildFromException($exception),
-                new ExceptionMechanism(ExceptionMechanism::TYPE_GENERIC, true)
+                $hint->mechanism ?? new ExceptionMechanism(ExceptionMechanism::TYPE_GENERIC, true)
             );
         } while ($exception = $exception->getPrevious());
 

tests/ClientTest.php
@@ -705,6 +705,42 @@ final class ClientTest extends TestCase
         ]));
     }
 
+    public function testBuildEventWithExceptionAndMechansim(): void
+    {
+        $options = new Options();
+        $exception = new \Exception('testMessage');
+
+        /** @var TransportInterface&MockObject $transport */
+        $transport = $this->createMock(TransportInterface::class);
+        $transport->expects($this->once())
+            ->method('send')
+            ->with($this->callback(function (Event $event): bool {
+                $capturedExceptions = $event->getExceptions();
+
+                $this->assertCount(1, $capturedExceptions);
+                $this->assertNotNull($capturedExceptions[0]->getStacktrace());
+                $this->assertEquals(new ExceptionMechanism(ExceptionMechanism::TYPE_GENERIC, false), $capturedExceptions[0]->getMechanism());
+                $this->assertSame(\Exception::class, $capturedExceptions[0]->getType());
+                $this->assertSame('testMessage', $capturedExceptions[0]->getValue());
+
+                return true;
+            }));
+
+        $client = new Client(
+            $options,
+            $transport,
+            'sentry.sdk.identifier',
+            '1.2.3',
+            new Serializer($options),
+            $this->createMock(RepresentationSerializerInterface::class)
+        );
+
+        $client->captureEvent(Event::createEvent(), EventHint::fromArray([
+            'exception' => $exception,
+            'mechanism' => new ExceptionMechanism(ExceptionMechanism::TYPE_GENERIC, false),
+        ]));
+    }
+
     public function testBuildWithErrorException(): void
     {
         $options = new Options();

tests/EventHintTest.php
@@ -6,6 +6,7 @@ namespace Sentry\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Sentry\EventHint;
+use Sentry\ExceptionMechanism;
 use Sentry\Frame;
 use Sentry\Stacktrace;
 
@@ -14,17 +15,20 @@ final class EventHintTest extends TestCase
     public function testCreateFromArray(): void
     {
         $exception = new \Exception();
+        $mechanism = new ExceptionMechanism(ExceptionMechanism::TYPE_GENERIC, false);
         $stacktrace = new Stacktrace([
             new Frame('function_1', 'path/to/file_1', 10),
         ]);
 
         $hint = EventHint::fromArray([
             'exception' => $exception,
+            'mechanism' => $mechanism,
             'stacktrace' => $stacktrace,
             'extra' => ['foo' => 'bar'],
         ]);
 
         $this->assertSame($exception, $hint->exception);
+        $this->assertSame($mechanism, $hint->mechanism);
         $this->assertSame($stacktrace, $hint->stacktrace);
         $this->assertSame(['foo' => 'bar'], $hint->extra);
     }
@@ -45,6 +49,11 @@ final class EventHintTest extends TestCase
         yield [
             ['exception' => 'foo'],
             'The value of the "exception" field must be an instance of a class implementing the "Throwable" interface. Got: "string".',
+        ];
+
+        yield [
+            ['mechanism' => 'foo'],
+            'The value of the "mechanism" field must be an instance of the "Sentry\\ExceptionMechanism" class. Got: "string".',
         ];
 
         yield [

commit 09ee993166a697dd34ff6e0ea8203b75f39a0e2a

fix: Add missing array key to docblock
Broken by 04d4280cd3e94ce7ff902dc0ae925f3607f11095

phpstan-baseline.neon
@@ -42,7 +42,7 @@ parameters:
 
 		-
 			message: "#^Result of && is always false\\.$#"
-			count: 2
+			count: 3
 			path: src/EventHint.php
 
 		-

src/EventHint.php
@@ -42,6 +42,7 @@ final class EventHint
      *
      * @psalm-param array{
      *     exception?: \Throwable|null,
+     *     mechanism?: ExceptionMechanism|null,
      *     stacktrace?: Stacktrace|null,
      *     extra?: array<string, mixed>
      * } $hintData