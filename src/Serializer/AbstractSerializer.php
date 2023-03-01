e641217959f5900d1941a413733383f254be3469
Adam Lutka <adam.lutka@gmail.com>
2023-03-01 16:59:18 +0100

    fix(serializer): serializeValue uses only scalar object IDs
    Handle object IDs that aren't scalars. It prevents php warning 'Array to string conversion'
    and similar.

--- src/Serializer/AbstractSerializer.php
+++ src/Serializer/AbstractSerializer.php
@@ -254,7 +254,7 @@ abstract class AbstractSerializer
                 }
             }
 
-            return 'Object ' . $reflection->getName() . ((null !== $objectId) ? '(#' . $objectId . ')' : '');
+            return 'Object ' . $reflection->getName() . (\is_scalar($objectId) ? '(#' . $objectId . ')' : '');
         }
 
         if (\is_resource($value)) {
