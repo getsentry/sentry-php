192e88ef9898cfdff1a97d9ac19c4c87cf2edc06
Michi Hoffmann <cleptric@users.noreply.github.com>
2023-03-14 21:38:21 +0100

    feat: Add support for Cron Monitoring (#1467)

--- src/Transport/HttpTransport.php
+++ src/Transport/HttpTransport.php
@@ -112,7 +112,10 @@ final class HttpTransport implements TransportInterface
             return new RejectedPromise(new Response(ResponseStatus::rateLimit(), $event));
         }
 
-        if (EventType::transaction() === $eventType) {
+        if (
+            EventType::transaction() === $eventType ||
+            EventType::checkIn() === $eventType
+        ) {
             $request = $this->requestFactory->createRequest('POST', $dsn->getEnvelopeApiEndpointUrl())
                 ->withHeader('Content-Type', 'application/x-sentry-envelope')
                 ->withBody($this->streamFactory->createStream($this->payloadSerializer->serialize($event)));
