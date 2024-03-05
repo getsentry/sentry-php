c5395f5e3c1554179677ccdd324daed03fce619e
Alex Bouma <alex@bouma.me>
2024-03-05 15:00:39 +0100

    Bring back removed tests

--- tests/Transport/HttpTransportTest.php
+++ tests/Transport/HttpTransportTest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Sentry\Tests\Transport;
 
+use PHPUnit\Framework\Constraint\StringMatchesFormatDescription;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
 use Sentry\Event;
 use Sentry\HttpClient\HttpClientInterface;
 use Sentry\HttpClient\Response;
@@ -18,6 +20,11 @@ use Symfony\Bridge\PhpUnit\ClockMock;
 final class HttpTransportTest extends TestCase
 {
     /**
+     * @var LoggerInterface&MockObject
+     */
+    private $logger;
+
+    /**
      * @var MockObject&HttpAsyncClientInterface
      */
     private $httpClient;
@@ -29,6 +36,7 @@ final class HttpTransportTest extends TestCase
 
     protected function setUp(): void
     {
+        $this->logger = $this->createMock(LoggerInterface::class);
         $this->httpClient = $this->createMock(HttpClientInterface::class);
         $this->payloadSerializer = $this->createMock(PayloadSerializerInterface::class);
     }
@@ -36,25 +44,36 @@ final class HttpTransportTest extends TestCase
     /**
      * @dataProvider sendDataProvider
      */
-    public function testSend(Response $response, ResultStatus $expectedResultStatus, bool $expectEventReturned): void
+    public function testSend(Response $response, ResultStatus $expectedResultStatus, bool $expectEventReturned, array $expectedLogMessages): void
     {
         $event = Event::createEvent();
 
         $this->payloadSerializer->expects($this->once())
-                                ->method('serialize')
-                                ->with($event)
-                                ->willReturn('{"foo":"bar"}');
+            ->method('serialize')
+            ->with($event)
+            ->willReturn('{"foo":"bar"}');
 
         $this->httpClient->expects($this->once())
-                         ->method('sendRequest')
-                         ->willReturn($response);
+            ->method('sendRequest')
+            ->willReturn($response);
+
+        foreach ($expectedLogMessages as $level => $messages) {
+            $this->logger->expects($this->exactly(\count($messages)))
+                ->method($level)
+                ->with($this->logicalOr(
+                    ...array_map(function (string $message) {
+                        return new StringMatchesFormatDescription($message);
+                    }, $messages)
+                ));
+        }
 
         $transport = new HttpTransport(
             new Options([
                 'dsn' => 'http://public@example.com/1',
             ]),
             $this->httpClient,
-            $this->payloadSerializer
+            $this->payloadSerializer,
+            $this->logger
         );
 
         // We need to mock the time to ensure that the rate limiter works as expected and we can easily assert the log messages
@@ -74,12 +93,24 @@ final class HttpTransportTest extends TestCase
             new Response(200, [], ''),
             ResultStatus::success(),
             true,
+            [
+                'info' => [
+                    'Sending event [%s] to %s [project:%s].',
+                    'Sent event [%s] to %s [project:%s]. Result: "success" (status: 200).',
+                ],
+            ],
         ];
 
         yield [
             new Response(401, [], ''),
             ResultStatus::invalid(),
             false,
+            [
+                'info' => [
+                    'Sending event [%s] to %s [project:%s].',
+                    'Sent event [%s] to %s [project:%s]. Result: "invalid" (status: 401).',
+                ],
+            ],
         ];
 
         ClockMock::withClockMock(1644105600);
@@ -88,12 +119,27 @@ final class HttpTransportTest extends TestCase
             new Response(429, ['Retry-After' => ['60']], ''),
             ResultStatus::rateLimit(),
             false,
+            [
+                'info' => [
+                    'Sending event [%s] to %s [project:%s].',
+                    'Sent event [%s] to %s [project:%s]. Result: "rate_limit" (status: 429).',
+                ],
+                'warning' => [
+                    'Rate limited exceeded for requests of type "event", backing off until "2022-02-06T00:01:00+00:00".',
+                ],
+            ],
         ];
 
         yield [
             new Response(500, [], ''),
             ResultStatus::failed(),
             false,
+            [
+                'info' => [
+                    'Sending event [%s] to %s [project:%s].',
+                    'Sent event [%s] to %s [project:%s]. Result: "failed" (status: 500).',
+                ],
+            ],
         ];
     }
 
@@ -102,21 +148,31 @@ final class HttpTransportTest extends TestCase
         $exception = new \Exception('foo');
         $event = Event::createEvent();
 
+        /** @var LoggerInterface&MockObject $logger */
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->expects($this->once())
+            ->method('error')
+            ->with(
+                new StringMatchesFormatDescription('Failed to send event [%s] to %s [project:%s]. Reason: "foo".'),
+                ['exception' => $exception, 'event' => $event]
+            );
+
         $this->payloadSerializer->expects($this->once())
-                                ->method('serialize')
-                                ->with($event)
-                                ->willReturn('{"foo":"bar"}');
+            ->method('serialize')
+            ->with($event)
+            ->willReturn('{"foo":"bar"}');
 
         $this->httpClient->expects($this->once())
-                         ->method('sendRequest')
-                         ->will($this->throwException($exception));
+            ->method('sendRequest')
+            ->will($this->throwException($exception));
 
         $transport = new HttpTransport(
             new Options([
                 'dsn' => 'http://public@example.com/1',
             ]),
             $this->httpClient,
-            $this->payloadSerializer
+            $this->payloadSerializer,
+            $logger
         );
 
         $result = $transport->send($event);
@@ -128,21 +184,31 @@ final class HttpTransportTest extends TestCase
     {
         $event = Event::createEvent();
 
+        /** @var LoggerInterface&MockObject $logger */
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->expects($this->once())
+            ->method('error')
+            ->with(
+                new StringMatchesFormatDescription('Failed to send event [%s] to %s [project:%s]. Reason: "cURL Error (6) Could not resolve host: example.com".'),
+                ['event' => $event]
+            );
+
         $this->payloadSerializer->expects($this->once())
-                                ->method('serialize')
-                                ->with($event)
-                                ->willReturn('{"foo":"bar"}');
+            ->method('serialize')
+            ->with($event)
+            ->willReturn('{"foo":"bar"}');
 
         $this->httpClient->expects($this->once())
-                         ->method('sendRequest')
-                         ->willReturn(new Response(0, [], 'cURL Error (6) Could not resolve host: example.com'));
+            ->method('sendRequest')
+            ->willReturn(new Response(0, [], 'cURL Error (6) Could not resolve host: example.com'));
 
         $transport = new HttpTransport(
             new Options([
                 'dsn' => 'http://public@example.com/1',
             ]),
             $this->httpClient,
-            $this->payloadSerializer
+            $this->payloadSerializer,
+            $logger
         );
 
         $result = $transport->send($event);
@@ -159,21 +225,31 @@ final class HttpTransportTest extends TestCase
 
         $event = Event::createEvent();
 
+        /** @var LoggerInterface&MockObject $logger */
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->expects($this->exactly(2))
+            ->method('warning')
+            ->withConsecutive(
+                ['Rate limited exceeded for requests of type "event", backing off until "2022-02-06T00:01:00+00:00".', ['event' => $event]],
+                ['Rate limit exceeded for sending requests of type "event".', ['event' => $event]]
+            );
+
         $this->payloadSerializer->expects($this->once())
-                                ->method('serialize')
-                                ->with($event)
-                                ->willReturn('{"foo":"bar"}');
+            ->method('serialize')
+            ->with($event)
+            ->willReturn('{"foo":"bar"}');
 
         $this->httpClient->expects($this->once())
-                         ->method('sendRequest')
-                         ->willReturn(new Response(429, ['Retry-After' => ['60']], ''));
+            ->method('sendRequest')
+            ->willReturn(new Response(429, ['Retry-After' => ['60']], ''));
 
         $transport = new HttpTransport(
             new Options([
                 'dsn' => 'http://public@example.com/1',
             ]),
             $this->httpClient,
-            $this->payloadSerializer
+            $this->payloadSerializer,
+            $logger
         );
 
         // Event should be sent, but the server should reply with a HTTP 429
