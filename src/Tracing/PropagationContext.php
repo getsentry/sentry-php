615606dd6e931d23880cdbda0e67c7d45896c4a7
Michael Hoffmann <michael.hoffmann@sentry.io>
2024-03-08 07:32:53 +0100

    Always add sampled flag to W3C trace-parent header

--- src/Tracing/PropagationContext.php
+++ src/Tracing/PropagationContext.php
@@ -72,7 +72,7 @@ final class PropagationContext
      */
     public function toW3CTraceparent(): string
     {
-        return sprintf('00-%s-%s', (string) $this->traceId, (string) $this->spanId);
+        return sprintf('00-%s-%s-00', (string) $this->traceId, (string) $this->spanId);
     }
 
     /**
