d2d30b9d40139d846ba2067b6d908d2053346eff
Alex Bouma <alex@bouma.me>
2025-01-25 14:47:39 +0100

    Set the current span on the hub in Guzzle middleware

--- src/Tracing/GuzzleTracingMiddleware.php
+++ src/Tracing/GuzzleTracingMiddleware.php
@@ -59,6 +59,8 @@ final class GuzzleTracingMiddleware
                     $spanContext->setDescription($request->getMethod() . ' ' . $partialUri);
 
                     $childSpan = $parentSpan->startChild($spanContext);
+
+                    $hub->setSpan($childSpan);
                 }
 
                 if (self::shouldAttachTracingHeaders($client, $request)) {
@@ -68,11 +70,13 @@ final class GuzzleTracingMiddleware
                         ->withHeader('baggage', getBaggage());
                 }
 
-                $handlerPromiseCallback = static function ($responseOrException) use ($hub, $spanAndBreadcrumbData, $childSpan, $partialUri) {
+                $handlerPromiseCallback = static function ($responseOrException) use ($hub, $spanAndBreadcrumbData, $childSpan, $parentSpan, $partialUri) {
                     if ($childSpan !== null) {
                         // We finish the span (which means setting the span end timestamp) first to ensure the measured time
                         // the span spans is as close to only the HTTP request time and do the data collection afterwards
                         $childSpan->finish();
+
+                        $hub->setSpan($parentSpan);
                     }
 
                     $response = null;
