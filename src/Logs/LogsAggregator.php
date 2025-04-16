3dde07e107f6f3112b7c53b3adddde8a157ba27d
Michael Hoffmann <michael.hoffmann@sentry.io>
2025-04-16 23:27:34 +0200

    Add some default attributes

--- src/Logs/LogsAggregator.php
+++ src/Logs/LogsAggregator.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Sentry\Logs;
 
+use Sentry\Client;
 use Sentry\Event;
 use Sentry\EventId;
 use Sentry\SentrySdk;
 use Sentry\State\Scope;
-use Sentry\Tracing\TransactionSource;
 
 /**
  * @internal
@@ -30,12 +30,64 @@ final class LogsAggregator
             $traceId = $span->getTraceId();
         }
 
-        $this->logs[] = [
+        $log = [
             'timestamp' => $timestamp,
             'trace_id' => (string) $traceId,
             'level' => (string) $level,
             'body' => $message,
+            'attributes' => [],
         ];
+
+        $hub = SentrySdk::getCurrentHub();
+        $client = $hub->getClient();
+
+        $hub = SentrySdk::getCurrentHub();
+        $client = $hub->getClient();
+
+        if ($client !== null) {
+            $options = $client->getOptions();
+
+            // @TODO add a proper attributes abstraction we can later re-use for spans
+            $defaultAttributes = [
+                'sentry.environment' => [
+                    'type' => 'string',
+                    'value' => $options->getEnvironment() ?? Event::DEFAULT_ENVIRONMENT,
+                ],
+                'sentry.sdk.name' => [
+                    'type' => 'string',
+                    // @FIXME Won't work for Laravel & Symfony
+                    'value' => Client::SDK_IDENTIFIER,
+                ],
+                'sentry.sdk.version' => [
+                    'type' => 'string',
+                    // @FIXME Won't work for Laravel & Symfony
+                    'value' => Client::SDK_VERSION,
+                ],
+                // @TODO Add a `server.address` attribute, same value as `server_name` on errors
+            ];
+
+            $release = $options->getRelease();
+            if ($release !== null) {
+                $defaultAttributes['sentry.release'] = [
+                    'type' => 'string',
+                    'value' => $options->getRelease(),
+                ];
+            }
+
+            $hub->configureScope(function (Scope $scope) use (&$defaultTags) {
+                $span = $scope->getSpan();
+                if ($span !== null) {
+                    $defaultAttributes['sentry.trace.parent_span_id'] = [
+                        'type' => 'string',
+                        'value' => $span->getSpanId(),
+                    ];
+                }
+            });
+
+            $log['attributes'] = $defaultAttributes;
+        }
+
+        $this->logs[] = $log;
     }
 
     public function flush(): ?EventId
@@ -50,47 +102,5 @@ final class LogsAggregator
         $this->logs = [];
 
         return $hub->captureEvent($event);
-    }
-
-    /**
-     * @param array<string, string> $tags
-     *
-     * @return array<string, string>
-     */
-    private function serializeTags(array $tags): array
-    {
-        $hub = SentrySdk::getCurrentHub();
-        $client = $hub->getClient();
-
-        if ($client !== null) {
-            $options = $client->getOptions();
-
-            $defaultTags = [
-                'environment' => $options->getEnvironment() ?? Event::DEFAULT_ENVIRONMENT,
-            ];
-
-            $release = $options->getRelease();
-            if ($release !== null) {
-                $defaultTags['release'] = $release;
-            }
-
-            $hub->configureScope(function (Scope $scope) use (&$defaultTags) {
-                $transaction = $scope->getTransaction();
-                if (
-                    $transaction !== null
-                    // Only include the transaction name if it has good quality
-                    && $transaction->getMetadata()->getSource() !== TransactionSource::url()
-                ) {
-                    $defaultTags['transaction'] = $transaction->getName();
-                }
-            });
-
-            $tags = array_merge($defaultTags, $tags);
-        }
-
-        // It's very important to sort the tags in order to obtain the same bucket key.
-        ksort($tags);
-
-        return $tags;
     }
 }
