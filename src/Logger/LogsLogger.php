bd543986f6eb7a9930bcdbdd19c83f511789e22d
Alex Bouma <alex@bouma.me>
2025-05-12 11:44:02 +0200

    Add a PSR logger that logs to logs

---
+++ src/Logger/LogsLogger.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sentry\Logger;
+
+use Psr\Log\AbstractLogger;
+use Sentry\Logs\LogAttribute;
+use Sentry\Logs\Logs;
+
+/**
+ * @phpstan-import-type AttributeValue from LogAttribute
+ */
+class LogsLogger extends AbstractLogger
+{
+    /**
+     * @param mixed              $level
+     * @param string|\Stringable $message
+     * @param mixed[]            $context
+     */
+    public function log($level, $message, array $context = []): void
+    {
+        // @TODO: The $context might contain attributes we don't support yet (like arrays for example), should we ignore them? Type hint is basically mixed[].
+        switch ($level) {
+            case 'emergency':
+            case 'critical':
+                // @phpstan-ignore-next-line
+                Logs::getInstance()->fatal((string) $message, [], $context);
+                break;
+            case 'error':
+                // @phpstan-ignore-next-line
+                Logs::getInstance()->error((string) $message, [], $context);
+                break;
+            case 'warning':
+                // @phpstan-ignore-next-line
+                Logs::getInstance()->warn((string) $message, [], $context);
+                break;
+            case 'debug':
+                // @phpstan-ignore-next-line
+                Logs::getInstance()->debug((string) $message, [], $context);
+                break;
+            default:
+                // @phpstan-ignore-next-line
+                Logs::getInstance()->info((string) $message, [], $context);
+                break;
+        }
+    }
+}
