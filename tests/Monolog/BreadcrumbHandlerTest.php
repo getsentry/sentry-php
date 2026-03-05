cc2f5ceced2ea0effcdd9c2a3b937fe2a0754f1d
Martin Seitz <martin.seitz@walls.io>
2026-03-05 12:26:40 +0100

    Add a test for timestamps

--- tests/Monolog/BreadcrumbHandlerTest.php
+++ tests/Monolog/BreadcrumbHandlerTest.php
@@ -21,11 +21,11 @@ final class BreadcrumbHandlerTest extends TestCase
         $hub = $this->createMock(HubInterface::class);
         $hub->expects($this->once())
             ->method('addBreadcrumb')
-            ->with($this->callback(function (Breadcrumb $breadcrumb) use ($expectedBreadcrumb, $record): bool {
+            ->with($this->callback(function (Breadcrumb $breadcrumb) use ($expectedBreadcrumb): bool {
                 $this->assertSame($expectedBreadcrumb->getMessage(), $breadcrumb->getMessage());
                 $this->assertSame($expectedBreadcrumb->getLevel(), $breadcrumb->getLevel());
                 $this->assertSame($expectedBreadcrumb->getType(), $breadcrumb->getType());
-                $this->assertEquals((float) $record['datetime']->format('U.u'), $breadcrumb->getTimestamp());
+                $this->assertEquals($expectedBreadcrumb->getTimestamp(), $breadcrumb->getTimestamp());
                 $this->assertSame($expectedBreadcrumb->getCategory(), $breadcrumb->getCategory());
                 $this->assertEquals($expectedBreadcrumb->getMetadata(), $breadcrumb->getMetadata());
 
@@ -41,12 +41,15 @@ final class BreadcrumbHandlerTest extends TestCase
      */
     public static function handleDataProvider(): iterable
     {
+        $now = new \DateTimeImmutable();
+
         $defaultBreadcrumb = new Breadcrumb(
             Breadcrumb::LEVEL_DEBUG,
             Breadcrumb::TYPE_DEFAULT,
             'channel.foo',
             'foo bar',
-            []
+            [],
+            (float) $now->format('U.u')
         );
 
         $levelsToBeTested = [
@@ -58,31 +61,46 @@ final class BreadcrumbHandlerTest extends TestCase
 
         foreach ($levelsToBeTested as $loggerLevel => $breadcrumbLevel) {
             yield 'with level ' . Logger::getLevelName($loggerLevel) => [
-                RecordFactory::create('foo bar', $loggerLevel, 'channel.foo', [], []),
+                RecordFactory::create('foo bar', $loggerLevel, 'channel.foo', [], [], $now),
                 $defaultBreadcrumb->withLevel($breadcrumbLevel),
             ];
         }
 
         yield 'with level ERROR' => [
-            RecordFactory::create('foo bar', Logger::ERROR, 'channel.foo', [], []),
+            RecordFactory::create('foo bar', Logger::ERROR, 'channel.foo', [], [], $now),
             $defaultBreadcrumb->withLevel(Breadcrumb::LEVEL_ERROR)
                 ->withType(Breadcrumb::TYPE_ERROR),
         ];
 
         yield 'with level ALERT' => [
-            RecordFactory::create('foo bar', Logger::ALERT, 'channel.foo', [], []),
+            RecordFactory::create('foo bar', Logger::ALERT, 'channel.foo', [], [], $now),
             $defaultBreadcrumb->withLevel(Breadcrumb::LEVEL_FATAL)
                 ->withType(Breadcrumb::TYPE_ERROR),
         ];
 
         yield 'with context' => [
-            RecordFactory::create('foo bar', Logger::DEBUG, 'channel.foo', ['context' => ['foo' => 'bar']], []),
+            RecordFactory::create('foo bar', Logger::DEBUG, 'channel.foo', ['context' => ['foo' => 'bar']], [], $now),
             $defaultBreadcrumb->withMetadata('context', ['foo' => 'bar']),
         ];
 
         yield 'with extra' => [
-            RecordFactory::create('foo bar', Logger::DEBUG, 'channel.foo', [], ['extra' => ['foo' => 'bar']]),
+            RecordFactory::create('foo bar', Logger::DEBUG, 'channel.foo', [], ['extra' => ['foo' => 'bar']], $now),
             $defaultBreadcrumb->withMetadata('extra', ['foo' => 'bar']),
+        ];
+
+        yield 'with timestamp' => [
+            RecordFactory::create('foo bar', Logger::DEBUG, 'channel.foo', [], [], new \DateTimeImmutable('1970-01-01 00:00:42.1337 UTC')),
+            $defaultBreadcrumb->withTimestamp(42.1337),
+        ];
+
+        yield 'with zero timestamp' => [
+            RecordFactory::create('foo bar', Logger::DEBUG, 'channel.foo', [], [], new \DateTimeImmutable('1970-01-01 00:00:00.000 UTC')),
+            $defaultBreadcrumb->withTimestamp(0.0),
+        ];
+
+        yield 'with negative timestamp' => [
+            RecordFactory::create('foo bar', Logger::DEBUG, 'channel.foo', [], [], new \DateTimeImmutable('1969-12-31 23:59:56.859 UTC')),
+            $defaultBreadcrumb->withTimestamp(-3.141),
         ];
     }
 }
