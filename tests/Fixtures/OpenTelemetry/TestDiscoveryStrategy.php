aefb3104a62d9735a5ab3fe6e40be4d27d8a5d33
Martin Linzmayer <martin.linzmayer@sentry.io>
2026-06-23 00:11:55 +0200

    test(otel): fix OTLP discovery setup

--- tests/Fixtures/OpenTelemetry/TestDiscoveryStrategy.php
+++ tests/Fixtures/OpenTelemetry/TestDiscoveryStrategy.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Sentry\Tests\Fixtures\OpenTelemetry;
 
+use GuzzleHttp\Psr7\HttpFactory;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
@@ -21,7 +22,10 @@ final class TestDiscoveryStrategy
         }
 
         if (is_a(RequestFactoryInterface::class, $type, true) || is_a(StreamFactoryInterface::class, $type, true)) {
-            return [['class' => Psr17Factory::class, 'condition' => Psr17Factory::class]];
+            return [
+                ['class' => HttpFactory::class, 'condition' => HttpFactory::class],
+                ['class' => Psr17Factory::class, 'condition' => Psr17Factory::class],
+            ];
         }
 
         return [];
